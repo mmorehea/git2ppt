8a50ac5068b2adcefadeae7503b314c67f65277c
Michael Morehead <mmorehea@mix.wvu.edu>
2016-10-18 16:23:15 -0400

    can extract appropiate git commits now

--- git2ppt.py
+++ git2ppt.py
@@ -3,8 +3,70 @@ import sys
 import code
 import numpy as np
 
-l = np.genfromtxt("try.txt")
+class Commit(object):
+	def __init__(self, author, date, commit_message):
+		self.author = author
+		self.date = date
+		self.commit_message = commit_message
 
 
 
-code.interact(local=locals())
+with open("try.txt", "r") as f:
+    content = f.readlines()
+
+lines = [line.rstrip('\n') for line in content]
+
+
+
+def buildCommits(lines):
+	commit = ""
+	author = ""
+	message = ""
+	date = ""
+	first = 0
+	allCommits = []
+
+	for each in lines:
+		if len(each) == 0:
+			continue
+		if each.split()[0] == 'commit':
+			if first != 0:
+				com = Commit(author, date, message)
+				allCommits.append(com)
+				commit = ""
+				author = ""
+				message = ""
+				date = ""
+			else:
+				commit = each
+				first = 1
+		elif each.split()[0] == 'Author:':
+			author = each
+		elif each.split()[0] == 'Date:':
+			date = each
+		elif each[0] == ':':
+			continue
+		else:
+			message += each + ' '
+	return allCommits
+
+def filterCommits(allCommits, authorName):
+	filteredCommits = []
+	for each in allCommits:
+		if authorName in each.author:
+			filteredCommits.append(each)
+	return filteredCommits
+
+def printCommits(allCommits):
+	for each in allCommits:
+		print each.author
+		print each.date
+		print ' '.join(each.commit_message.split())
+		print " "
+		print " "
+
+
+allCommits = buildCommits(lines)
+filtered = filterCommits(allCommits, "Michael")
+
+printCommits(filtered)
