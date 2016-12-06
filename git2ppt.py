323c3cb84e8915921e6e710aa665add1a837c69d
Michael Morehead <mmorehea@mix.wvu.edu>
2016-12-06 14:27:23 -0500

    now writes out filtered commits

--- git2ppt.py
+++ git2ppt.py
@@ -3,7 +3,6 @@ import sys
 import code
 import numpy as np
 import subprocess
-from pptx import Presentation
 
 class Commit(object):
 	def __init__(self, author, date, commit_message):
@@ -75,7 +74,18 @@ def printCommits(allCommits):
 		print " "
 		print " "
 
+def writeCommit(commits):
+	f = open("filteredCommits.txt", 'w')
+	for each in commits:
+		f.write(each.author + '\r\n')
+		f.write(each.date + '\r\n')
+		f.write(each.commit_message + '\r\n')
+		f.write('\r\n')
+		f.write('\r\n')
+
+
 def buildPPTX(commits):
+	from pptx import Presentation
 	prs = Presentation()
 	title_slide_layout = prs.slide_layouts[0]
 	slide = prs.slides.add_slide(title_slide_layout)
@@ -86,7 +96,7 @@ def buildPPTX(commits):
 	subtitle.text = "Using git2ppt!"
 
 	prs.save('test.pptx')
-
+	commits.reverse()
 	for ii,each in enumerate(commits):
 		print ii
 		bullet_slide_layout = prs.slide_layouts[5]
@@ -99,17 +109,25 @@ def buildPPTX(commits):
 	prs.save('test.pptx')
 
 def main():
+	print "git2ppt -- Written by Michael Morehead Nov. 2016"
+	print "usage: python git2ppt.py [path/to/repo] [makePPT = 0]"
+	print " set makePPT to 1 to try to make a ppt"
 	if len(sys.argv) < 2:
-		print "git2ppt -- Written by Michael Morehead Nov. 2016"
-		print "usage: python git2ppt.py path/to/repo"
+		sys.exit(0)
 	else:
+		makePPT = 0
+		if len(sys.argv) == 3:
+			makePPT = sys.argv[2]
 		pathToRepo = sys.argv[1]
 		getCommits(pathToRepo)
 		allCommits = buildCommits()
 		filtered = filterCommits(allCommits, "mmorehea")
 
 		printCommits(filtered)
-		buildPPTX(filtered)
+		writeCommit(filtered)
+
+		if makePPT:
+			buildPPTX(filtered)
 
 
 if __name__ == "__main__":
